fea62f73b916bcc8effa56f173205b3b5a8d3a87
Krzysi3k <global.takeover@o2.pl>
2017-09-11 13:03:03 +0200

    Update approve_pending_management.ps1

--- approve_pending_management/approve_pending_management.ps1
+++ approve_pending_management/approve_pending_management.ps1
@@ -7,6 +7,7 @@ $pmgmt = Get-SCOMPendingManagement
 $output = "C:\Temp\output.log"
 $approved = "approved.log"
 $notapproved = "not_approved.log"
+$blacklist = Get-Content "$currpth\blacklist.txt"
 $body = @()
 
 if($pmgmt)
@@ -18,11 +19,29 @@ if($pmgmt)
     $body += $agents | % {Write-Output $_`n}
     foreach($mgmt in $pmgmt)
     {
+        [bool]$flag = $false
         $conn = (Test-Connection $mgmt.AgentName -ErrorAction SilentlyContinue)
         if($conn)
         {
-            "approving $($mgmt.AgentName)" | Add-Content $currpth\$approved
-            Approve-SCOMPendingManagement -PendingAction $mgmt
+			foreach($b in $blacklist)
+            {
+                if($mgmt.AgentName -match $b)
+                {
+                    [bool]$flag = $true
+                }
+            }
+
+            if($flag -eq $true)
+            {
+                # flag = true = blacklisted
+				$body += "`nfound machine from blacklist:`n$($mgmt.AgentName)"
+				"pending NOT approved $($mgmt.AgentName)" | Add-Content $currpth\$notapproved
+            }
+            else
+            {
+                "approving $($mgmt.AgentName)" | Add-Content $currpth\$approved
+				Approve-SCOMPendingManagement -PendingAction $mgmt
+			}
         }
         Else
         {
